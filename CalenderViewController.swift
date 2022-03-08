4dbaf040334e5736b1f7a1b00bf85d92ce07112b
Ren <ren.t21@icloud.com>
2022-03-08 14:15:41 +0900

    first commit

---
+++ CalenderViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalenderApp
+//
+//  Created by 高橋蓮 on 2022/03/08.
+//
+
+import UIKit
+
+class CalenderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
